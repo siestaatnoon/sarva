ac0cf5c96af7386b011a3dbd11bed9548fc36faa
siestaatnoon <siestaatnoon@users.noreply.github.com>
2019-11-17 08:39:48 +0100

    Remove old Nearby Messages p2p classes

--- app/src/main/java/com/cccdlabs/sarva/presentation/di/modules/P2pModule.java
+++ app/src/main/java/com/cccdlabs/sarva/presentation/di/modules/P2pModule.java
@@ -2,11 +2,6 @@ package com.cccdlabs.sarva.presentation.di.modules;
 
 import android.app.Activity;
 
-import com.cccdlabs.sarva.data.p2p.nearby.NearbyPartnerBroadcast;
-import com.cccdlabs.sarva.data.p2p.nearby.NearbyPartnerCheck;
-import com.cccdlabs.sarva.data.p2p.nearby.NearbyPartnerFind;
-import com.cccdlabs.sarva.data.p2p.nearby.NearbyPartnerSearch;
-import com.cccdlabs.sarva.data.p2p.nearby.NearbyPartnerTransmitter;
 import com.cccdlabs.sarva.data.p2p.nearby.PartnerBroadcastEmitter;
 import com.cccdlabs.sarva.data.p2p.nearby.PartnerCheckEmitter;
 import com.cccdlabs.sarva.data.p2p.nearby.PartnerFindEmitter;
@@ -56,31 +51,5 @@ public class P2pModule {
     @Provides @PerActivity
     PartnerTransmitterEmitter providePartnerTransmitterEmitter() {
         return new PartnerTransmitterEmitter(activity, repository);
-    }
-
-
-    @Provides @PerActivity
-    NearbyPartnerBroadcast provideNearbyPartnerBroadcast() {
-        return new NearbyPartnerBroadcast(activity);
-    }
-
-    @Provides @PerActivity
-    NearbyPartnerCheck provideNearbyPartnerCheck() {
-        return new NearbyPartnerCheck(activity, repository);
-    }
-
-    @Provides @PerActivity
-    NearbyPartnerFind provideNearbyPartnerFind() {
-        return new NearbyPartnerFind(activity, repository);
-    }
-
-    @Provides @PerActivity
-    NearbyPartnerSearch provideNearbyPartnerSearch() {
-        return new NearbyPartnerSearch(activity, repository);
-    }
-
-    @Provides @PerActivity
-    NearbyPartnerTransmitter provideNearbyPartnerTransmitter() {
-        return new NearbyPartnerTransmitter(activity, repository);
     }
 }
--- app/src/test/java/com/cccdlabs/sarva/presentation/di/modules/TestP2pModule.java
+++ app/src/test/java/com/cccdlabs/sarva/presentation/di/modules/TestP2pModule.java
@@ -1,10 +1,5 @@
 package com.cccdlabs.sarva.presentation.di.modules;
 
-import com.cccdlabs.sarva.data.p2p.nearby.NearbyPartnerBroadcast;
-import com.cccdlabs.sarva.data.p2p.nearby.NearbyPartnerCheck;
-import com.cccdlabs.sarva.data.p2p.nearby.NearbyPartnerFind;
-import com.cccdlabs.sarva.data.p2p.nearby.NearbyPartnerSearch;
-import com.cccdlabs.sarva.data.p2p.nearby.NearbyPartnerTransmitter;
 import com.cccdlabs.sarva.data.p2p.nearby.PartnerBroadcastEmitter;
 import com.cccdlabs.sarva.data.p2p.nearby.PartnerCheckEmitter;
 import com.cccdlabs.sarva.data.p2p.nearby.PartnerFindEmitter;
@@ -40,26 +35,5 @@ public class TestP2pModule {
 
     @Provides @PerActivity PartnerTransmitterEmitter providePartnerTransmitterEmitter() {
         return mock(PartnerTransmitterEmitter.class);
-    }
-
-
-    @Provides @PerActivity NearbyPartnerBroadcast provideNearbyPartnerBroadcast() {
-        return mock(NearbyPartnerBroadcast.class);
-    }
-
-    @Provides @PerActivity NearbyPartnerCheck provideNearbyPartnerCheck() {
-        return mock(NearbyPartnerCheck.class);
-    }
-
-    @Provides @PerActivity NearbyPartnerFind provideNearbyPartnerFind() {
-        return mock(NearbyPartnerFind.class);
-    }
-
-    @Provides @PerActivity NearbyPartnerSearch provideNearbyPartnerSearch() {
-        return mock(NearbyPartnerSearch.class);
-    }
-
-    @Provides @PerActivity NearbyPartnerTransmitter provideNearbyPartnerTransmitter() {
-        return mock(NearbyPartnerTransmitter.class);
     }
 }
